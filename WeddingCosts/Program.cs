25fece961a27c6f017f0b5a4d1e3821ea81f0c4d
Sagar Gill <sagargill@hotmail.co.uk>
2021-06-08 21:28:46 +0100

    Add project files.

---
+++ WeddingCosts/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace WeddingCosts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UI.UserInterface();
+
+            var userInput = Console.ReadLine();
+
+            UserInput.UserInputLetter(userInput);
+
+        }
+
+        
+
+    }
+    }
+
+
---
+++ WeddingCosts/UI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeddingCosts
+{
+    public class UI
+    {
+        public static void UserInterface()
+        {
+            Console.WriteLine("Type a code from the list below to view costs:");
+            Console.WriteLine("Bandstand (b)");
+            Console.WriteLine("Venue (v)");
+            Console.WriteLine("Food (f)");
+            Console.WriteLine("Cake (c)");
+            Console.WriteLine("Drinks (d)");
+            Console.WriteLine("Decor (de)");
+            Console.WriteLine("Music (m)");
+            Console.WriteLine("Hair (h)");
+            Console.WriteLine("Make-Up (mu)");
+            Console.WriteLine("Photographer (p)");
+            Console.WriteLine("Total (t)");
+            Console.WriteLine("Quit (q)");
+        }
+    }
+}
