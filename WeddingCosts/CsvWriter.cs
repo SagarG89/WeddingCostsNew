757fa2d0c7755087444f7be31d5a53cd66c3d545
Sagar Gill <sagargill@hotmail.co.uk>
2021-06-10 10:52:21 +0100

    Add function included

---
+++ WeddingCosts/CsvWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using CsvHelper;
+
+//namespace WeddingCosts
+//{
+//    class CsvWriter
+//    {
+//        private string _csvFilePath;
+
+//        public CsvWriter(string csvFilePath)
+//        {
+//            this._csvFilePath = csvFilePath;
+//        }
+
+//        //public Dictionary<string, List<BookingCosts>> AddBookingCost()
+        //{
+        //    var costs = new Dictionary<string, List<BookingCosts>>();
+
+        //    using StreamWriter sw = new StreamWriter(_csvFilePath))
+        //        {
+        //        sw.ReadLine();
+
+        //        string bookCostLine;
+        //        while ((bookCostLine = sw.ReadLine()) !=null)
+        //        {
+//        //            BookingCosts.booking = AddToCsv(bookCostLine);
+//        //            File.
+//        //        }
+//            }
+//        }
+
+
+//    }
+//}
--- WeddingCosts/Program.cs
+++ WeddingCosts/Program.cs
@@ -22,36 +22,84 @@ namespace WeddingCosts
 
 
             //I've used some help from previous tutorial code to write this up
+
             string filePath = @"C:\Users\sagar\source\repos\WeddingCosts\WeddingCosts\BookCost.csv";
             CsvReader reader = new CsvReader(filePath);
-            bool userWantsToExit = false;
+            bool readOrAddCost = false;
+            bool userWantsToViewCosts = false;
+            bool userWantsToAddCost = false;
+           
+            
 
+           
             Dictionary<string, List<BookingCosts>> bookings = reader.ReadBookingCosts();
             foreach (string booking in bookings.Keys)
-                Console.WriteLine(booking);
+                Console.WriteLine($"{booking}");
+            Console.WriteLine("-------------------------------");
 
-            while (!userWantsToExit)
+            while (!readOrAddCost)
             {
-                Console.Write("Which of the costs do you wish to see? ");
-                string chosenBooking = Console.ReadLine();
-
-                if (bookings.ContainsKey(chosenBooking))
-                {
-                    foreach (BookingCosts booking in bookings[chosenBooking].Take(1))
-                        Console.WriteLine($"The cost of {booking.Booking} is {booking.Cost}");
-                }
-                else
-                {
-                    Console.WriteLine("That is not a valid cost");
-                }
-                Console.WriteLine("Press Q to quit or any key to continue");
+                Console.WriteLine("Press V to View Costs or A to add.");
                 string userChoice = Console.ReadLine();
-                    if (userChoice == "Q")
+
+                if (userChoice == "V")
                 {
-                    userWantsToExit = true;
+                    while (!userWantsToViewCosts)
+                    {
+                        Console.Write("Which of the costs do you wish to see? ");
+                        string chosenBooking = Console.ReadLine();
+
+                        if (bookings.ContainsKey(chosenBooking))
+                        {
+                            foreach (BookingCosts booking in bookings[chosenBooking].Take(1))
+                                Console.WriteLine($"The cost of {booking.Booking} is {booking.Cost}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("That is not a valid cost");
+                        }
+                        Console.WriteLine("Press Q to quit or any key to continue");
+                        string userQuitView = Console.ReadLine();
+                        if (userQuitView == "Q")
+                        {
+                            userWantsToViewCosts = true;
+                            readOrAddCost = true;
+                        }
+
+                    }
+
                 }
-           
+
+                if (userChoice == "A")
+                {
+                    Console.WriteLine("Please write the Name and Cost (ex. Name, Cost)");
+                    string newName = Console.ReadLine();
+
+                    //adds to file but want to add above Total. Also does not show after when trying to view.
+                    while (!userWantsToAddCost)
+                    {
+                        using (FileStream fs = new FileStream(filePath, FileMode.Append, FileAccess.Write))
+                        {
+                            using (StreamWriter sw = new StreamWriter(fs))
+                            {
+                                sw.WriteLine(newName);
+                            }
+                        }
+                        Console.WriteLine("Press Q to quit or any key to continue");
+                        string userQuitAdd = Console.ReadLine();
+                        if (userQuitAdd == "Q")
+                        {
+                            userWantsToAddCost = true;
+                            readOrAddCost = true;
+                        }
+                    }
+                }
+
             }
+         
+
+
+           
         }
     }
 }
